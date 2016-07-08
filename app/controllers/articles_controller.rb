18af6f7246d0c88aa69193179179225a44f4ec31
max1on1 <2630161@gmail.com>
2016-07-09 01:02:38 +0300

    1

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,21 @@
 class ArticlesController < ApplicationController
-	def new
+	
+  def new
 	end
 	
+  def create
+    @article = Article.new(article_params)
+    if @article.valid?
+      @article.save
+      redirect_to @article
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
