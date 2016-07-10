47370ecd7dddafc2ae44b28c4b02a5ab3d4c9ba1
Maxim Glotov <Maxim Glotov>
2016-07-10 16:40:09 +0300

    implement article#show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,11 @@
 class ArticlesController < ApplicationController
 	
   def new
+     @article = Article.new
 	end
-	
+	def show
+    @article = Article.find(params[:id])
+  end
   def create
     @article = Article.new(article_params)
     if @article.valid?
