0a2158199527479f02646628923e6a4dfab7a170
Maxim Glotov <Maxim Glotov>
2016-07-10 19:17:45 +0300

    implement articles#edit and #update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,13 +5,15 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
      @article = Article.new
 	end
 	
-  def show
-    @article = Article.find(params[:id])
-  end
+  
   
   def create
     @article = Article.new(article_params)
@@ -20,6 +22,21 @@ class ArticlesController < ApplicationController
       redirect_to @article
     else
       render action: 'new'
+    end
+  end
+
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.valid?
+      @article.update(article_params)
+      redirect_to @article
+    else
+      render action: 'edit'
     end
   end
 
